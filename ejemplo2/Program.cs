3ffa6794940028b98c437df99799af66ecd657cb
Bryan Sandoval <bsandovalr1@miumg.edu.gt>
2024-02-17 15:46:43 -1200

    primer commit

---
+++ ejemplo2/Program.cs
@@ -0,0 +1,26 @@
+﻿ushort num1, num2, resultado3;
+
+resultado3 = 0;
+
+
+try
+{
+
+    Console.WriteLine("Ingrese un numero: ");
+    num1 = Convert.ToByte(Console.ReadLine());
+
+    Console.WriteLine("Ingrese un numero: ");
+    num2 = Convert.ToByte(Console.ReadLine());
+
+    resultado3 = (ushort)(num1 * num2);
+    Console.WriteLine($"El resultado es: {resultado3}");
+
+}
+
+catch (Exception ex)
+{
+
+
+
+
+}
